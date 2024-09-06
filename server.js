36fd198ecc0d07c023bdb4b7de7d6c0207b3730d
Davidson-Tavares <davidsonandre1@gmail.com>
2024-09-05 22:20:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express'
 const app = express()
+app.use(express.json)
 
 app.get('/usuarios', (req, res) => {
     res.send('Primeira rota funcionando !')
+})
+
+app.post('/usuarios', (req, res) => {
+    res.send('ok deu certo !')
+
 })
 
 app.listen(3000)
